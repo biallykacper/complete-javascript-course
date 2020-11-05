50776e4eea4fa233c53759bce65a6685a4ea3a66
biallykacper <69823293+biallykacper@users.noreply.github.com>
2020-11-06 00:59:09 +0100

    fundamentals

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,4 +1,39 @@
+/*
+Valuables and Variables
+
 let js = "amazing";
-if (js === "amazing") alert("JS is fun!");
-40 - 8 + 23 - 10;
+
 console.log(40 - 8 + 23 - 10);
+
+console.log("Jonas");
+console.log(23);
+
+// Valuables and Variables task 1
+let country = "Poland";
+let continent = "Europe";
+let population = 37.97 + " milions";
+
+console.log(country);
+console.log(continent);
+console.log(population);
+*/
+/*
+Data Types
+let jsIsFun = true;
+console.log(jsIsFun);
+
+console.log(typeof jsIsFun);
+console.log(typeof 23);
+console.log(typeof "Kacper");
+jsIsFun = "yes!";
+console.log(typeof jsIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 2000;
+console.log(typeof year);
+
+console.log(typeof null);
+*/
